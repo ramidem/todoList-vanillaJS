d481d2aa92b6399a83ed05f1bbd0774ca51de1d6
Med Aduh <im.ramidem@gmail.com>
2019-03-01 08:04:50 +0800

    Version 3
    - todoList Object
    - displayTodos method
    - addTodo method
    - changeTodo method
    - removeTodo method

--- README.md
+++ README.md
@@ -14,7 +14,15 @@
 
 **Version 2 - Functions**
 
-- It should have a function to display todos
-- It should have a function to add new todos
-- It should have a function to change a todo
-- It should have a function to delete a todo
+- It should have a `function` to display todos
+- It should have a `function` to add new todos
+- It should have a `function` to change a todo
+- It should have a `function` to delete a todo
+
+**Version 2 - Objects**
+
+- It should store the todos array on an `object`
+- It should have a `displayTodos` method
+- It should have a `addTodo` method
+- It should have a `changeTodo` method
+- It should have a `deleteTodo` method
--- todo.js
+++ todo.js
@@ -1,27 +1,33 @@
 // It should have a place to store todos
-var todos = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+var todoList = {
+  todos: ["item 1", "item 2", "item 3", "item 4", "item 5"],
 
-// display todos
-function displayTodos() {
-  return `${todos.length} Todos: ${todos}`;
-}
+  // display todos
+  displayTodos: function() {
+    return this.todos.length + " TODOS: " + this.todos;
+  },
 
-// add todos
-function addTodos(todo) {
-  todos.push(todo);
-}
+  // add a todo
+  addTodo: function(todo) {
+    this.todos.push(todo);
+  },
 
-// change a todo
-function changeTodo(index, newTodo) {
-  todos[index] = newTodo;
-}
+  // change a todo
+  changeTodo: function(index, newTodo) {
+    this.todos[index] = newTodo;
+  },
 
-// delete a todo
-function removeTodo(index) {
-  todos.splice(index, 1);
-}
+  // delete a todo
+  removeTodo: function(index) {
+    this.todos.splice(index, 1);
+  }
+};
 
-addTodos("get shit done!");
-changeTodo(0, "first todo");
-removeTodo(3);
-console.log(displayTodos());
+console.log(todoList.displayTodos());
+
+console.log(todoList.addTodo("Get shit done"));
+console.log(todoList.changeTodo(4, "Get shit done"));
+console.log(todoList.removeTodo(0));
+console.log(todoList.removeTodo(0));
+
+console.log(todoList.displayTodos());
